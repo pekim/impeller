333cb183f7c8ab486afc259c7bfab45cf067ed12
Mike D Pilsbury <mike@pekim.co.uk>
2026-08-15 15:37:01 +0100

    Move extracted downloaded files to internal/interop
    And retain more extracted files, notably the licence.

---
+++ internal/interop/LICENSE.sdk.md
@@ -0,0 +1,7 @@
+# _sdk_license_md
+
+This bundle is part of the the Flutter SDK.
+
+The source code is hosted at [flutter/engine/impeller/toolkit/interop](https://github.com/flutter/engine/tree/4d3c9c58afe01bf68fae7b002918b014e2da90c7/impeller/toolkit/interop).
+The license for this bundle is hosted at [flutter/engine/sky/packages/sky_engine/LICENSE](https://github.com/flutter/engine/tree/4d3c9c58afe01bf68fae7b002918b014e2da90c7/sky/packages/sky_engine/LICENSE) 
+and [sky_engine.zip](https://storage.googleapis.com/flutter_infra_release/flutter/4d3c9c58afe01bf68fae7b002918b014e2da90c7/sky_engine.zip).
--- internal/update.sh
+++ internal/update.sh
@@ -2,22 +2,18 @@
 
 FLUTTER_SHA=4d3c9c58afe01bf68fae7b002918b014e2da90c7
 PLATFORM_ARCH=linux-x64
-DEST_DIR=internal
+DEST_DIR=internal/interop
 TEMP_DIR=temp
 ZIP_FILE=$TEMP_DIR/impeller_sdk.zip
 
 mkdir -p $DEST_DIR
 mkdir -p $TEMP_DIR
 
-# Sownload an archive with prebuilt artifacts.
+# Download an archive with prebuilt artifacts.
 wget \
   --no-verbose \
   --output-document $ZIP_FILE \
   https://storage.googleapis.com/flutter_infra_release/flutter/$FLUTTER_SHA/$PLATFORM_ARCH/impeller_sdk.zip
 
-# Extract the C header file and the shared library.
-unzip \
-  $ZIP_FILE \
-  include/impeller.h \
-  lib/libimpeller.so \
-  -d $DEST_DIR
+# Extract the C header file, pre-build library.
+unzip $ZIP_FILE -d $DEST_DIR
