bce49d6ae7e29187739ecdb97438a03ccaafefee
Mike D Pilsbury <mike@pekim.co.uk>
2026-08-15 15:26:59 +0100

    Add a script to download and extract artifacts
    The script will download an archive with prebuilt artifacts, and
    extract the header and shared library.

---
+++ internal/update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FLUTTER_SHA=4d3c9c58afe01bf68fae7b002918b014e2da90c7
+PLATFORM_ARCH=linux-x64
+DEST_DIR=internal
+TEMP_DIR=temp
+ZIP_FILE=$TEMP_DIR/impeller_sdk.zip
+
+mkdir -p $DEST_DIR
+mkdir -p $TEMP_DIR
+
+# Sownload an archive with prebuilt artifacts.
+wget \
+  --no-verbose \
+  --output-document $ZIP_FILE \
+  https://storage.googleapis.com/flutter_infra_release/flutter/$FLUTTER_SHA/$PLATFORM_ARCH/impeller_sdk.zip
+
+# Extract the C header file and the shared library.
+unzip \
+  $ZIP_FILE \
+  include/impeller.h \
+  lib/libimpeller.so \
+  -d $DEST_DIR
