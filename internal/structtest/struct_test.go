48c5d30e915742418956663e3baf4feab93613a6
Mike D Pilsbury <mike@pekim.co.uk>
2026-08-17 12:15:00 +0100

    Make struct test in to subtests

--- internal/structtest/struct_test.go
+++ internal/structtest/struct_test.go
@@ -11,28 +11,30 @@ import (
 
 func TestStructs(t *testing.T) {
 	for _, struct_ := range structs {
-		cValue := reflect.ValueOf(struct_.c)
-		goValue := reflect.ValueOf(struct_.go_)
+		t.Run(struct_.name, func(t *testing.T) {
+			cValue := reflect.ValueOf(struct_.c)
+			goValue := reflect.ValueOf(struct_.go_)
 
-		assert.Equal(t, cValue.Type().Size(), goValue.Type().Size(),
-			fmt.Sprintf("%q size", struct_.name))
+			assert.Equal(t, cValue.Type().Size(), goValue.Type().Size(),
+				fmt.Sprintf("%q size", struct_.name))
 
-		cFields := reflect.VisibleFields(cValue.Type())
-		cFields = removeAnonymousFields(cFields)
-		goFields := reflect.VisibleFields(goValue.Type())
-		goFields = removeAnonymousFields(goFields)
-		assert.Equal(t, len(cFields), len(goFields),
-			fmt.Sprintf("%s fields count", struct_.name))
+			cFields := reflect.VisibleFields(cValue.Type())
+			cFields = removeAnonymousFields(cFields)
+			goFields := reflect.VisibleFields(goValue.Type())
+			goFields = removeAnonymousFields(goFields)
+			assert.Equal(t, len(cFields), len(goFields),
+				fmt.Sprintf("%s fields count", struct_.name))
 
-		for i := range cFields {
-			cField := cFields[i]
-			goField := goFields[i]
+			for i := range cFields {
+				cField := cFields[i]
+				goField := goFields[i]
 
-			assert.Equal(t, cField.Offset, goField.Offset,
-				fmt.Sprintf("%s.%s offset", struct_.name, cField.Name))
-			assert.Equal(t, cField.Type.Size(), goField.Type.Size(),
-				fmt.Sprintf("%s.%s size", struct_.name, cField.Name))
-		}
+				t.Run(cField.Name, func(t *testing.T) {
+					assert.Equal(t, cField.Offset, goField.Offset, "offset")
+					assert.Equal(t, cField.Type.Size(), goField.Type.Size(), "size")
+				})
+			}
+		})
 	}
 }
 
