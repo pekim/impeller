df00e3859ff439e9d35f4d5389a914b0f5d6bd5e
Mike D Pilsbury <mike@pekim.co.uk>
2026-08-18 09:31:21 +0100

    Move scalar definitions in to typ.go file

--- internal/generate/typ.go
+++ internal/generate/typ.go
@@ -5,6 +5,43 @@ import (
 	"github.com/go-clang/clang-v15/clang"
 )
 
+type scalar struct {
+	goType         jen.Code
+	typeDescriptor jen.Code
+}
+
+var scalars = map[clang.CursorKind]scalar{
+	clang.Type_Bool: {
+		goType:         jen.Id("Bool"),
+		typeDescriptor: jen.Qual(typesImportPath, "UInt8TypeDescriptor"),
+	},
+
+	clang.Type_Float: {
+		goType:         jen.Float32(),
+		typeDescriptor: jen.Qual(typesImportPath, "FloatTypeDescriptor"),
+	},
+
+	clang.Type_Int: {
+		goType:         jen.Int32(),
+		typeDescriptor: jen.Qual(typesImportPath, "SInt32TypeDescriptor"),
+	},
+
+	clang.Type_Long: {
+		goType:         jen.Int64(),
+		typeDescriptor: jen.Qual(typesImportPath, "SInt64TypeDescriptor"),
+	},
+
+	clang.Type_UInt: {
+		goType:         jen.Uint32(),
+		typeDescriptor: jen.Qual(typesImportPath, "UInt32TypeDescriptor"),
+	},
+
+	clang.Type_ULong: {
+		goType:         jen.Uint64(),
+		typeDescriptor: jen.Qual(typesImportPath, "UInt64TypeDescriptor"),
+	},
+}
+
 type typ struct {
 	typ      clang.Type
 	isEnum   bool
