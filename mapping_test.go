d898fabf165fad458325a89a16ed6bfb1f1e1f8f
Mike D Pilsbury <mike@pekim.co.uk>
2026-08-18 20:35:05 +0100

    Use more realistic user data in mapping test

--- mapping_test.go
+++ mapping_test.go
@@ -14,20 +14,21 @@ var font []byte // a nice small font, that won't bloat the git repo
 func TestMappingOnRelease(t *testing.T) {
 	assert.NoError(t, Init())
 
-	expectedUserData := unsafe.Pointer(uintptr(42))
+	userData := 42
 	released := false
+
 	mapping := &Mapping{
 		Data:   unsafe.Pointer(&font[0]),
 		Length: uint64(len(font)),
 		OnRelease: NewCallback(func(user_data unsafe.Pointer) {
-			assert.Equal(t, expectedUserData, user_data)
+			assert.Equal(t, userData, *(*int)(user_data))
 			released = true
 		}),
 	}
 
 	typographyContext := TypographyContextNew()
 	assert.NotNil(t, typographyContext.handle)
-	typographyContext.RegisterFont(mapping, unsafe.Pointer(uintptr(42)), "somefont")
+	typographyContext.RegisterFont(mapping, unsafe.Pointer(&userData), "somefont")
 	typographyContext.Release()
 	assert.True(t, released)
 }
