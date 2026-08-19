fe85568f350293e1d8bc0d22c3d88fd07c9d16f4
Mike D Pilsbury <mike@pekim.co.uk>
2026-08-19 09:14:21 +0100

    Make running tests more readable

--- generate.go
+++ generate.go
@@ -1,4 +1,5 @@
 package impeller
 
 //go:generate go run internal/generate/cmd/main.go
-//go:generate go test -count=1 github.com/pekim/impeller github.com/pekim/impeller/internal/structtest
+//go:generate go test -count=1 github.com/pekim/impeller
+//go:generate go test -count=1 github.com/pekim/impeller/internal/structtest
