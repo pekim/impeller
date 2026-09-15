2b383e64eaf31c137f9c9db223d657b39a475835
Mike D Pilsbury <mike@pekim.co.uk>
2026-09-15 16:48:03 +0100

    Add a minimal package documentation comment

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package impeller provides cgo-free bindings for Flutter's [Impeller].
+
+[Impeller]: https://github.com/flutter/flutter/tree/master/engine/src/flutter/impeller
+*/
+package impeller
