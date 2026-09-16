1920a520c1504d4b641f88d75ab37a4707a17ce3
Mike D Pilsbury <mike@pekim.co.uk>
2026-09-16 06:18:37 +0100

    Add a note about generated documentation comments

--- doc.go
+++ doc.go
@@ -1,6 +1,10 @@
 /*
 Package impeller provides cgo-free bindings for Flutter's [Impeller].
 
+The documentation is generated from comments in the C header that defines the Impeller C API.
+It will not always quite match the generated Go API,
+and it may not always make sense in the context of Go.
+
 [Impeller]: https://github.com/flutter/flutter/tree/master/engine/src/flutter/impeller
 */
 package impeller
